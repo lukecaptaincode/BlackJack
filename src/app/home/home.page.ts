4cedd121c66bf6958a71d8d6ab741047de42c7e6
lukecaptaincode <lukecaptaincode@gmail.com>
2021-11-12 16:33:00 +0000

    Started UI

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,12 +1,26 @@
 import { Component } from '@angular/core';
-
+import { Blackjack } from '../classes/BlackJack';
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
 })
 export class HomePage {
-
+  blackJack: Blackjack;
   constructor() {}
+
+  start(){
+    this.blackJack = new Blackjack();
+  }
+
+  hit(){
+    this.blackJack.hit(this.blackJack.player);
+    this.blackJack.handCheck(this.blackJack.player);
+  }
+
+  stand(){
+    this.blackJack.stand();
+    this.blackJack.handCheck(this.blackJack.dealer);
+  }
 
 }
